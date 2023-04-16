087d05e8c76b6e61f560792bfbf428f6d867c587
35kartal35 <mozdemir3545@gmail.com>
2023-04-16 19:45:24 +0300

    second commit

---
+++ 2-types/6-type-keyword.ts
@@ -0,0 +1,102 @@
+/**
+ * Typescript bize tür oluşturma imkanı verir.
+ * bu işlemi yapabilmek için type, interface, class keywordlerini kullanabiliriz.
+ * her keywordün kendine ait özellikleri vardır.
+ */
+
+/**
+ * yeni bir type (tür) tanımlarken bazı kurallar vardır:
+ * -Type'ların ismi Pascal case olmalı
+ * -type ların sonuna "Type" eklenirse daha anlamlı olur.
+ */
+
+type User = {
+    id: number,
+    firstname: string,
+    lastname: string
+    //
+};
+
+/**
+ * değişken tanımlayıp bunu değer ataması yapılırken kullanılan kurallar:
+ *  - Değişken isimleri snake case veya camel case olmalı. (Camel case olursa daha iyi olur).
+ */
+
+const has = {
+    id: 1,
+    firstname: "ahmet",
+    lastname: "hakan",
+    //
+};
+
+// yukarıda oluşturduğumuz "user" türünü kullanalım.
+
+const user_1: User = {
+    id: 1,
+    firstname: "ramazan",
+    lastname: "özbuğanlı",
+}
+
+const user_2: User = {
+    id: 2,
+    firstname: "furkan",
+    lastname: "güneş",
+};
+
+const user_3: User = {
+    firstname: "mehmet",
+    lastname: "özdemir",
+    id: 3
+};
+
+const user_4: User = {
+    firstname: "Ömer Said",
+    id: 4,
+    lastname: "özdemir",
+}
+
+
+type Student = {
+
+    firstname: string,
+    lastname: string,
+    no: number,
+};
+
+const student_1: Student = {
+    firstname: "Devrim",
+    lastname: "tarhan",
+    no: 43
+
+
+};
+
+const student_2: Student = {
+    firstname: "yusuf",
+    lastname: "kenan",
+    no: 34
+};
+
+const student_3: Student = {
+    firstname: "Mehmet",
+    lastname: "Çimen",
+    no: 35
+};
+
+
+
+
+
+type vosvos = {
+    model: number,
+    nickname: string,
+    birth: Date,
+};
+
+const vosvos_1: vosvos = {
+    birth: new Date(1987 - 9 - 12),
+    model: 1987,
+    nickname: "halo"
+};
+
+console.log(vosvos_1)
